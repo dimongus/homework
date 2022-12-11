200e39b6acd7cc1d47200b235eac49f7b0fbd68c
dmitrii <dima1994.dl@gmail.com>
2022-12-11 13:13:01 +0900

    23#

---
+++ seminar3/23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите чилсо");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("_____");
+int[] arr = GetCalculateTheCubeOfnumbers(num);
+
+PrintArray(arr);
+
+int[] GetCalculateTheCubeOfnumbers(int N)
+{
+    int[] array = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = (i + 1) * (i + 1) * (i + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}

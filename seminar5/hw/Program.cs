14d57f23ee86f7688afb1cb7a98512c1d8c329dd
dmitrii <dima1994.dl@gmail.com>
2022-12-15 21:16:24 +0900

    sem5

--- seminar5/hw/Program.cs
+++ seminar5/hw/Program.cs
@@ -21,9 +21,58 @@
 // ________________________________________________________________
 
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
 // 452 -> 11
-
 // 82 -> 10
-
 // 9012 -> 12
+
+
+// Console.WriteLine("Введите число ");
+// int n = int.Parse(Console.ReadLine());
+//   int sumN(int n)
+//   {
+    
+//     int counter = Convert.ToString(n).Length;
+//     int advance = 0;
+//     int result = 0;
+
+//     for (int i = 0; i < counter; i++){
+//       advance = n - n % 10;
+//       result = result + (n - advance);
+//       n = n / 10;
+//     }
+//    return result;
+//   }
+
+// int sumNumber = sumN(n);
+// Console.WriteLine($"Сумма цифр в числе {sumNumber}");
+
+// ____________________________________________________
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+
+
+
+Console.WriteLine("Введите ряд чисел, разделенных запятой : ");
+int num = int.Parse(Console.ReadLine());
+
+int [] numbers = new int[num];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = numbers [i]+1;
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
